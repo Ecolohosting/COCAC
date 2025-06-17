18811da36f28b5a505c42c38389c0baaab08bbf6
Ecolohosting <39418701+Ecolohosting@users.noreply.github.com>
2025-06-17 14:29:09 -0700

    Add reorder and delete controls to gallery

--- loginwp/wp-content/plugins/cocac-gallery/assets/js/admin.js
+++ loginwp/wp-content/plugins/cocac-gallery/assets/js/admin.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function ($) {
     var frame;
+
+    function updateInput() {
+        var ids = [];
+        $('.cocac-gallery-preview li').each(function () {
+            ids.push($(this).data('id'));
+        });
+        $('#cocac_gallery_image_ids').val(ids.join(','));
+    }
+
+    $('.cocac-gallery-preview').sortable({
+        update: updateInput
+    }).on('click', '.cocac-remove-image', function () {
+        $(this).closest('li').remove();
+        updateInput();
+    });
+
     $('.cocac-add-gallery-images').on('click', function (e) {
         e.preventDefault();
         if (frame) {
@@ -12,17 +28,16 @@ jQuery(document).ready(function ($) {
             multiple: true
         });
         frame.on('select', function () {
-            var ids = [];
-            var preview = $('.cocac-gallery-preview').empty();
             frame.state().get('selection').each(function (attachment) {
-                ids.push(attachment.id);
-                if (attachment.attributes.sizes && attachment.attributes.sizes.thumbnail) {
-                    preview.append('<li data-id="' + attachment.id + '"><img src="' + attachment.attributes.sizes.thumbnail.url + '" /></li>');
-                } else {
-                    preview.append('<li data-id="' + attachment.id + '"><img src="' + attachment.url + '" /></li>');
+                if ($('.cocac-gallery-preview li[data-id="' + attachment.id + '"]').length === 0) {
+                    var url = attachment.url;
+                    if (attachment.attributes.sizes && attachment.attributes.sizes.thumbnail) {
+                        url = attachment.attributes.sizes.thumbnail.url;
+                    }
+                    $('.cocac-gallery-preview').append('<li data-id="' + attachment.id + '"><img src="' + url + '" /><button type="button" class="cocac-remove-image">&times;</button></li>');
                 }
             });
-            $('#cocac_gallery_image_ids').val(ids.join(','));
+            updateInput();
         });
         frame.open();
     });
